823425406b5bb959dc7a025023a90aefda15bc67
michaelx0281 <69123340+michaelx0281@users.noreply.github.com>
2023-11-06 16:54:31 -0500

    creating left, right motor profiles

--- src/main/java/org/sciborgs1155/robot/Ports.java
+++ src/main/java/org/sciborgs1155/robot/Ports.java
@@ -10,16 +10,8 @@ public final class Ports {
   }
 
   public static final class Drive {
-    public static final int FRONT_LEFT_DRIVE = 1;
-    public static final int REAR_LEFT_DRIVE = 38;
-    public static final int FRONT_RIGHT_DRIVE = 32;
-    public static final int REAR_RIGHT_DRIVE = 5;
-
-    public static final int FRONT_LEFT_TURNING = 31;
-    public static final int REAR_LEFT_TURNING = 36;
-    public static final int FRONT_RIGHT_TURNING = 3;
-    public static final int REAR_RIGHT_TURNING = 40;
-
-    public static final int PIGEON = 42;
+    // needs to be changed sometime soon
+    public static final int RdrivePort = -1;
+    public static final int LdrivePort = -1;
   }
 }
---
+++ src/main/java/org/sciborgs1155/robot/drive/driveMotor.java
@@ -0,0 +1,29 @@
+package org.sciborgs1155.robot.drive;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+
+import org.sciborgs1155.lib.constants.SparkUtils;
+import org.sciborgs1155.robot.Ports.Drive;
+
+public class driveMotor {
+
+    CANSparkMax Rmotor = SparkUtils.create(
+        Drive.RdrivePort,
+            s -> {
+              s.setInverted(false);
+              s.setIdleMode(IdleMode.kBrake);
+              s.setOpenLoopRampRate(0);
+              s.setSmartCurrentLimit(50);
+            });
+    
+    CANSparkMax Lmotor = SparkUtils.create(
+        Drive.LdrivePort,
+            s -> {
+              s.setInverted(false);
+              s.setIdleMode(IdleMode.kBrake);
+              s.setOpenLoopRampRate(0);
+              s.setSmartCurrentLimit(50);
+            });
+        
+}
